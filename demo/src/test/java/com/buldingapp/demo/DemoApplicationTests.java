a6eae176f24a58bd9b9cdc8bea4dc59bc30a2fca
Burdianov Iacov <iacov.burdianov@gmail.com>
2023-02-03 09:43:00 +0200

    first commit

---
+++ demo/src/test/java/com/buldingapp/demo/DemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.buldingapp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
